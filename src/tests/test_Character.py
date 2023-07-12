8ac53f7be566c9d520946198ec4f2fedf41bf76b
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-07-12 13:51:41 +0000

    added code for character test file

---
+++ src/tests/test_Character.py
@@ -0,0 +1,18 @@
+from Character import Character
+# Feature : Create a Character
+# As a character I want to have a name so that I can be distinguished from other characters
+# Can get a set Name
+
+def test_get_name():
+    c = Character()
+    assert c.name == 'Cletus'
+
+def test_set_name():
+    d = Character()
+    d.set_name('Joe')
+    assert d.name == 'Joe'
+
+def test_set_name2():
+    d = Character()
+    d.set_name('Joe')
+    assert d.name == 'Joe'
