26d3229dd22dd9591ff26f07e7a49fee7e29cb7d
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-07-12 15:58:38 +0000

    Created canAttack files for next feature

---
+++ src/tests/test_CanAttack.py
@@ -0,0 +1,9 @@
+from CanAttack import CanAttack
+
+#As a combatant I want to be able to attack other combatants so that I can survive to fight another day
+#Roll a 20 sided die
+#Roll must meet or beat opponents armor class to hit
+#A roll of 20 always hits
+
+def test_get_dice_roll():
+    
