a6a038a5602f26bdba28c075021358616731c355
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-07-13 13:54:04 +0000

    refactored CanAttack class into Character class, deleted duplicate code'

--- src/Iteration_One/Character.py
+++ src/Iteration_One/Character.py
@@ -8,41 +8,22 @@ class Character:
         self.armor = armor
         self.hitpoints = hitpoints
 
-    #takes in a string
+    #takes in a string 
     def set_name(self, n):
         self.name = n
     
     def set_alignment(self, alignment):
         self.alignment = alignment
+
+    #This function checks if a dice roll is higher than the defender's armor class
+    #If it is then the character can hit the defender
+    def check_attack(self, roll, defender):
+        self.roll = roll
+        canAttack = False
+        if self.roll > defender.armor:
+            canAttack = True
+        else:
+            canAttack = False
+        return canAttack
     
-
-
-
-
-
-'''
-class Character:
-    def __init__(self, name, health, strength):
-        self.name = name
-        self.health = health
-        self.strength = strength
-
-    def attack(self, target):
-        damage = self.strength
-        target.take_damage(damage)
-
-    def take_damage(self, damage):
-        self.health -= damage
-        if self.health <= 0:
-            self.health = 0
-            print(self.name + " has been defeated!")
-
-    def heal(self, amount):
-        self.health += amount
-
-    def display_status(self):
-        print("Name:", self.name)
-        print("Health:", self.health)
-        print("Strength:", self.strength)
-'''
 
